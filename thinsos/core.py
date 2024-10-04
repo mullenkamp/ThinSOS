e4c5275454e4267fc8bfc8ce2cae0f5c1e57f501
Mike Kittridge <mgkittridge@gmail.com>
2024-10-04 17:02:05 +1300

    added SOSish

--- thinsos/core.py
+++ thinsos/core.py
@@ -416,3 +416,219 @@ class SOS(object):
 
         return big_df
 
+#####################################################
+### Crappy non-standard SOS service by ORC...
+
+
+class SOSish(object):
+    """
+    SOS class specifically for 52 North SOS. Initialised with a url string.
+    """
+    def __init__(self, url, extra_params={"service": "SOS", "version": "2.0.0"}, token=''):
+        """
+
+        """
+        self.url = url
+        self.token = str(token)
+        self.headers = {'Authorization': str(token), 'Accept': 'application/json'}
+        self.body_base = extra_params
+        self.data_availability = self.get_data_availability()
+
+
+    def _url_convert(self, body):
+        """
+        Sends a request to a SOS using GET method.
+
+        Parameters
+        -----------
+        body : dict
+            body of the request.
+        url: str
+            URL to the endpoint where the SOS can be accessed
+
+        Returns
+        -------
+        requests response
+            Server response to response formatted as JSON
+        """
+        new_body = urllib.parse.urlencode(body, quote_via=urllib.parse.quote)
+
+        if self.url.endswith('/'):
+            url = self.url[:-1]
+        else:
+            url = self.url
+
+        new_url = url + '/?' + new_body
+
+        return new_url
+
+
+    def filters(self, offering=None, from_date=None, to_date=None):
+        """
+
+        """
+        da1 = self.data_availability.copy()
+        body = self.body_base.copy()
+
+        if isinstance(offering, str):
+            da1 = da1[da1.offering == offering]
+            if da1.empty:
+                raise ValueError('offering does not exist')
+            body.update({'offering': offering})
+        if self.request == 'GetObservation':
+            if isinstance(from_date, str):
+                from_date1 = pd.Timestamp(from_date).isoformat() + 'Z'
+            else:
+                from_date1 = da1.fromDate.min().strftime('%Y-%m-%dT%H:%M:%SZ')
+            if isinstance(to_date, str):
+                to_date1 = pd.Timestamp(to_date).isoformat() + 'Z'
+            else:
+                to_date1 = da1.toDate.min().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+#            tf = {"temporalFilter": {
+#                    "during": {
+#                        "ref": "om:phenomenonTime",
+#                        "value": [
+#                            from_date1,
+#                            to_date1
+#                            ]
+#                        }
+#                    }
+#                }
+            tf = {"temporalFilter": "om:phenomenonTime," + from_date1 + '/' + to_date1}
+            body.update(tf)
+
+        return body
+
+
+    def get_data_availability(self, foi=None, procedure=None, observed_property=None):
+        """
+
+        """
+        if (foi is None) & (procedure is None) & (observed_property is None):
+            body = self.body_base.copy()
+        else:
+            body = self.filters(foi, procedure, observed_property)
+        body.update({'request': 'GetDataAvailability'})
+
+        new_url = self._url_convert(body)
+
+        response = requests.get(new_url, headers=self.headers)
+
+        try:
+            response.raise_for_status()  # raise HTTP errors
+            json1 = response.json()['dataAvailability']
+        except Exception as err:
+            print(err)
+            return response
+
+        df1 = pd.DataFrame(json1)
+        df1[['fromDate', 'toDate']] = pd.DataFrame(df1['phenomenonTime'].values.tolist(), columns=['fromDate', 'toDate'])
+        df1.fromDate = pd.to_datetime(df1.fromDate)
+        df1.toDate = pd.to_datetime(df1.toDate)
+
+        return df1.drop('phenomenonTime', axis=1)
+
+
+    def get_foi(self, foi=None, bbox=None):
+        """
+
+        """
+        if (foi is None):
+            body = self.body_base.copy()
+        else:
+            body = self.filters(foi)
+        body.update({'request': 'GetFeatureOfInterest'})
+
+        new_url = self._url_convert(body)
+
+        response = requests.get(new_url, headers=self.headers)
+
+        try:
+            response.raise_for_status()  # raise HTTP errors
+        except Exception as err:
+            print(err)
+            return response
+
+        json1 = response.json()['featureOfInterest']
+
+        lst1 = [foi_parse(j) for j in json1 if isinstance(j, dict)]
+        df1 = pd.DataFrame(lst1)
+
+        ## bbox select
+        if isinstance(bbox, list):
+            df1 = df1[(df1.lon >= bbox[0][0]) & (df1.lat >= bbox[0][1]) & (df1.lon <= bbox[1][0]) & (df1.lat <= bbox[1][1])].copy()
+
+        return df1
+
+
+    def get_observation(self, offering, from_date=None, to_date=None, retries=5):
+        """
+
+        """
+        ### Chunk up the requests for the sake of the source server
+        da1 = self.data_availability.copy()
+        da1 = da1[(da1.offering == offering)].iloc[0]
+
+        if isinstance(from_date, str):
+            from_date1 = pd.Timestamp(from_date)
+        else:
+            from_date1 = da1.fromDate.tz_localize(None)
+        if isinstance(to_date, str):
+            to_date1 = pd.Timestamp(to_date)
+        else:
+            to_date1 = da1.toDate.tz_localize(None)
+
+        if (to_date1 - from_date1).days < (366*24):
+            dr2 = [str(from_date1), str(to_date1)]
+        else:
+            dr1 = pd.date_range(from_date1, to_date1, freq='60M')
+            dr2 = [str(d) for d in dr1]
+            dr2[0] = str(from_date1)
+            dr2[-1] = str(to_date1)
+
+        ### Iterate through each date range
+        self.request = 'GetObservation'
+
+        df_list = []
+        for i, d in enumerate(dr2[:-1]):
+            print(d)
+            from1 = d
+            to1 = dr2[i+1]
+
+            body = self.filters(offering, from1, to1)
+            body.update({'request': 'GetObservation'})
+
+            new_url = self._url_convert(body)
+
+            counter = retries
+            while counter > 0:
+                try:
+                    response = requests.get(new_url, headers=self.headers, timeout=150)
+                    break
+                except:
+                    print('Failed to extract data...trying again in 20 seconds...')
+                    counter = counter - 1
+                    sleep(20)
+                    if counter == 0:
+                        raise ValueError('Too many retries..something is wrong with the request.')
+
+            response.raise_for_status()  # raise HTTP errors
+
+            json1 = response.json()['observations']
+
+            if json1:
+                df1 = obs_process(json1)
+                df_list.append(df1)
+
+        if df_list:
+            big_df1 = pd.concat(df_list)
+            if 'phenomenonTime' in big_df1:
+                big_df = big_df1.drop_duplicates('phenomenonTime')
+            else:
+                big_df = big_df1.drop_duplicates('resultTime')
+        else:
+            big_df = pd.DataFrame()
+
+        return big_df
+
